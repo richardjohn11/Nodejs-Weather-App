93951ffb8fafe5b342cfcf368d09fd1aea03b489
Richard John Garcia <richardjohngarcia@yahoo.com>
2019-03-05 12:31:07 -0700

    Add GET /weather API and render it to Index.hbs

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const msgOne = document.querySelector("#msg-1");
+const msgTwo = document.querySelector("#msg-2");
+
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const location = search.value;
+  msgOne.textContent = "Loading...";
+  msgTwo.textContent = "";
+
+  fetch(`/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        msgOne.textContent = data.error;
+      } else {
+        msgOne.textContent = data.location;
+        msgTwo.textContent = data.forecast;
+      }
+    });
+  });
+});
--- src/app.js
+++ src/app.js
@@ -34,9 +34,27 @@ app.get("/weather", (req, res) => {
   if (!req.query.address) {
     return res.send({ error: "You must provide an address" });
   }
-  res.send({
-    title: "Help"
-  });
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address
+        });
+      });
+    }
+  );
 });
 
 app.get("*", (req, res) => {
