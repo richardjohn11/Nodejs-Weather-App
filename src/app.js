9a3bac2c5140a9dae39c726292d2b0187e19a75f
richard john garcia <richardjohngarcia@gmail.com>
2019-03-05 10:24:53 -0700

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const geocode = require("../utils/geocode");
+const forecast = require("../utils/forecast");
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+
+const app = express();
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../templates/views"));
+hbs.registerPartials(path.join(__dirname, "../templates/partials"));
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Chad"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About"
+  });
+});
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help"
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({ error: "You must provide an address" });
+  }
+  res.send({
+    title: "Help"
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", { title: "404 Page not Found" });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running...");
+});
